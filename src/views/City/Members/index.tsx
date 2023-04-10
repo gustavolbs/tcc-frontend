d2f744d6225ede704db5dfd129144eacd494130c
gustavolbs <gustavo.luiz.bispo.santos@gmail.com>
2023-04-10 11:51:02 -0300

    fix search members filter width

--- src/views/City/Members/index.tsx
+++ src/views/City/Members/index.tsx
@@ -44,14 +44,14 @@ export const CityMembers: React.FC = () => {
   return (
     <>
       <h2>Gerenciar Gestores</h2>
-      <span className="w-1/2 text-center mb-4">
+      <span className="w-full sm:w-1/2 text-center mb-4">
         Lembre-se: todos os usuários da sua cidade estão sendo exibidos aqui.
         Atribua os cargos aos usuários que deseja que sejam gestores de sua
         cidade.
       </span>
 
       {members?.length && !isLoadingMembers && (
-        <LabelLayout htmlFor="email" className="w-1/2">
+        <LabelLayout htmlFor="email" className="w-full sm:w-1/2">
           <ReactSVG src={emailSVG} />
           <input
             type="text"
